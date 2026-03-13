56709429e12b3b8520806a175799ccd8dea7dbfe
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-03-13 14:44:21 +0530

    operator

---
+++ opertaors.js
@@ -0,0 +1,52 @@
+let a = 5;
+let b = 2;
+console.log ("a =", a, "b =", b);
+
+// arithmetic operators
+// console.log("a + b =", a + b); // addition
+// console.log("a - b =", a - b); // subtraction
+// console.log("a * b =", a * b); // multiplication
+// console.log("a / b =", a / b); // division
+// console.log("a % b =", a % b); // modulus
+
+// unary operators
+// console.log("a++ =", a++); // post-increment
+// console.log("a =", a); // a is now 6
+
+// console.log("++b =", ++b); // pre-increment
+// console.log("b =", b); // b is now 3
+
+// console.log("a-- =", a--); // post-decrement
+// console.log("a =", a); // a is now 5
+
+// console.log("--b =", --b); // pre-decrement
+// console.log("b =", b); // b is now 2 
+
+// assignment operators
+// a += 4; // a = a + 4
+// console.log("a =", a); // a is now 9
+// a -= 5; // a = a - 5
+// console.log("a =", a); // a is now 4
+// a *= 3; // a = a * 3
+// console.log("a =", a); // a is now 12
+// a /= 2; // a = a / 2
+// console.log("a =", a); // a is now 6
+// a %= 4; // a = a % 4
+// console.log("a =", a); // a is now 2
+// a **=5; // a = a ** 5
+// console.log("a =", a); // a is now 32
+
+// comparison operators
+// console.log("a == b:", a == b); // equality
+// console.log("a != b:", a != b); // inequality
+// console.log("a === b:", a === b); // strict equality
+// console.log("a !== b:", a !== b); // strict inequality
+// console.log("a > b:", a > b); // greater than
+// console.log("a < b:", a < b); // less than
+// console.log("a >= b:", a >= b); // greater than or equal to
+// console.log("a <= b:", a <= b); // less than or equal to 
+
+// logical operators
+console.log("cond1 && cond2:", a > b &&  a === 5); // logical AND
+console.log("cond1 || cond2:", a < b || a === 5); // logical OR
+console.log("!cond1:", !(a === 9)); // logical NOT
