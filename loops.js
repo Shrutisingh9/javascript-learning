1cc44a29df957cac3497d219c1e792a56ee29412
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-03-16 14:59:07 +0530

    for loops

---
+++ loops.js
@@ -0,0 +1,16 @@
+// for loops
+// for (let i=1; i<=50000; i ++){
+//     console.log("Shruti");
+// }
+
+// Calculate sum of first n numbers
+// let sum = 0;
+// for (let i = 1; i<=5; i++){
+//     sum = sum  + i;
+// }
+// console.log("Sum =", sum);
+
+// Print 1 to 5
+for (let i = 1; i<=5; i ++){
+    console.log("i =" , i);
+}
