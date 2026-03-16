c5bca414b68fbded54558dfb5dc453f988096c41
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-03-16 14:39:56 +0530

    practice Question

---
+++ practice ques.js
@@ -0,0 +1,26 @@
+// Get user to input a number using prompt("Enter a number:"). Check if the number is multiple of 5 or not.
+
+// let num = prompt("Enter a number:");
+// if (num % 5 === 0){
+//     console.log(num , "is a multiple of 5.");
+// } else {
+//     console.log(num , "is not a multiple of 5.");
+
+// }
+
+// Write a code which can give grades to students according to their scores: 90-100, A  70-89, B  60-69, C  50-59, D  0-49, F
+
+// let score = prompt("Enter your score:");
+// if (score >= 90 && score <=100){
+//     console.log("Your grade is A.");    
+// } else if (score >= 70 && score <=89){
+//     console.log("Your grade is B.");
+// } else if (score >= 60 && score <=69){
+//     console.log("Your grade is C.");
+// } else if (score >= 50 && score <=59){
+//     console.log("Your grade is D.");
+// } else if (score >= 0 && score <=49){
+//     console.log("Your grade is F.");
+// } else {
+//     console.log("Invalid score. Please enter a score between 0 and 100.");
+// }
