0f5cb03d64ab1c79a1e4ccd55bab05b9c6fa7971
Shrutisingh9 <tanukum.ss784@gmail.com>
2026-04-13 13:56:39 +0530

    promise

--- advance js.js
+++ advance js.js
@@ -33,20 +33,108 @@
 // setTimeout(hello, 4000);
 
 // callback hell
+// function getData(dataId, getNextData){
+//     setTimeout(()=>{
+//         console.log("data", dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+        
+//     }, 2000);
+// }
+
+// getData(1,()=>{
+//     getData(2, () =>{
+//         getData(3);
+//     })
+// });
+
+// promises
+// let promise = new Promise((resolve, reject) =>{
+//     console.log("i am promise");
+//     resolve(1234)
+// });
+
+// function getData(dataId, getNextData){
+//     return new Promise ((resolve, reject) =>{
+//          setTimeout(()=>{
+//         // console.log("data", dataId);
+//         // resolve("success")
+//         // reject("error")
+//         if(getNextData){
+//             getNextData();
+//         }
+        
+//     }, 5000);
+// });
+// }
+// let result = getData(123)
+
+// .then() and .catch()
+// const getPromise = () =>{
+//     return new Promise((resolve, reject) =>{
+//     console.log("I am promise");
+//     // resolve("Sucess")
+//     // reject("Error")
+// });
+// };
+
+// let promise = getPromise();
+// promise.then((res)=>{
+//     console.log("Promise fulfilled", res)
+// });
+
+// promise.catch((err)=>{
+//     console.log("Rejected", err)
+// });
+
+// promise chain
+// function asyncFunc1(){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             console.log("data 1");
+//             resolve("Success");
+//         }, 3000);
+//     });
+// }
+
+// function asyncFunc2(){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             console.log("data 2");
+//             resolve("Success");
+//         }, 3000);
+//     });
+// }
+
+// console.log("fetching data 1....")
+// asyncFunc1().then((res) =>{
+//     console.log("fetching data 2....")
+//     asyncFunc2().then((res) =>{});
+// });
+
+
 function getData(dataId, getNextData){
-    setTimeout(()=>{
+    return new Promise ((resolve, reject) =>{
+         setTimeout(()=>{
         console.log("data", dataId);
+        resolve("success")
+        // reject("error")
         if(getNextData){
             getNextData();
         }
         
     }, 2000);
+});
 }
 
-getData(1,()=>{
-    getData(2, () =>{
-        getData(3);
+getData(1)
+    .then((res) =>{
+        return getData(2);
     })
-});
-
-// promises
+    .then((res) =>{
+        return getData(3); 
+    })
+    .then ((res) =>{
+        console.log(res);
+    });
